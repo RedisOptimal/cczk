bdb359f164e3c5a844bb4bd215d9bfd196b06678
Zhe Yuan <yizer16@gmail.com>
2013-11-01 15:43:58 +0800

    temporal commit

--- include/wather.h
+++ include/wather.h
@@ -11,19 +11,35 @@
 
 #ifndef _CCZK_ZK_WATHER_H_
 #define _CCZK_ZK_WATHER_H_
+#include <string>
+
 #include <boost/noncopyable.hpp>
+#include <boost/function.hpp>
+#include <boost/thread/mutex.hpp>
+
+#include <zookeeper_const.h>
 
 namespace cczk {
   using boost::noncopyable;
   class Wathcer : noncopyable {
   public  :
-    Wathcer() {
-      live = true;
+    typedef boost::function<void(const std::string&, WatchEvent::type)> Listener;
+    explicit Wathcer(Wathcer::Listener &listener) {
+      _live = true;
+      _listener = listener;
     }
-     
-  private :
-    bool live;
     
+    void close() {
+      _live = false;
+    }
+    
+    bool is_live() {
+      return _live;
+    }
+    
+  private :
+    bool _live;
+    Listener _listener;
   };
 } // namespace cczk
 
