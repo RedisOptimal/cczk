07dccb78dccd5ba102a2162c7eb4f20018eea9e4
Zhe Yuan <yizer16@gmail.com>
2013-11-08 20:54:26 +0800

    add test cases

--- test/zkclient_test.cc
+++ test/zkclient_test.cc
@@ -22,59 +22,71 @@ TEST(ZKCLIENT, OPEN) {
 }
 
 TEST(ZKCLIENT, CLOSE) {
+  using namespace cczk;
   
 }
 
 TEST(ZKCLIENT, CLEAR) {
+  using namespace cczk;
   
 }
 
 TEST(ZKCLIENT, IS_AVALIABLE) {
+  using namespace cczk;
   
 }
 
 TEST(ZKCLIENT, GET_CHILDREN_OF_PATH) {
+  using namespace cczk;
   
   
 }
 
 TEST(ZKCLIENT, SET_DATA_OF_NODE) {
+  using namespace cczk;
   
   
 }
 
 TEST(ZKCLIENT, GET_DATA_OF_NODE) {
+  using namespace cczk;
   
 }
 
 TEST(ZKCLIENT, CREATE_NODE) {
+  using namespace cczk;
   
   
 }
 
 
 TEST(ZKCLIENT, DELETE_NODE) {
+  using namespace cczk;
   
   
 }
 
 TEST(ZKCLIENT, EXIST) {
+  using namespace cczk;
   
 }
 
 TEST(ZKCLIENT, ADD_LISTENER) {
+  using namespace cczk;
   
   
 }
 
 
 TEST(ZKCLIENT, DROP_LISTENER) {
+  using namespace cczk;
   
   
 }
 
 
 TEST(ZKCLIENT, DROP_LISTENER_WITH_PATH) {
+  using namespace cczk;
   
   
 }
--- test/zookeeper_config_test.cc
+++ test/zookeeper_config_test.cc
@@ -2,8 +2,17 @@
 #include <gtest/gtest.h>
 
 TEST(ZOOKEEPER_CONFIG, CONSTRUCTION) {
-  
-  
+  using namespace cczk;
+  zookeeper_config config;
+  zookeeper_config config2("host", 3000, "/root");
+  zookeeper_config config3("host", 3000, "root");
+  ASSERT_EQ(config.get_host(), "");
+  ASSERT_EQ(config.get_root(), "");
+  ASSERT_EQ(config.get_session_timeout(), 0);
+  ASSERT_EQ(config2.get_host(), "host");
+  ASSERT_EQ(config2.get_root(), "root");
+  ASSERT_EQ(config2.get_session_timeout(), 3000);
+  ASSERT_EQ(config3.get_root(), "root");
 }
 
 TEST(ZOOKEEPER_CONFIG, LOAD_FROM_FILE) {
