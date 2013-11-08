18c26e473d8786d241d62722d5c85c1105c2860d
Zhe Yuan <yizer16@gmail.com>
2013-11-08 15:30:30 +0800

    update test cases

--- test/zkclient_open_test.cc
+++ test/zkclient_open_test.cc
@@ -5,5 +5,18 @@
 
 TEST(ZKCLIENT, OPEN) {
   using namespace cczk;
-  zookeeper_config config;
+  zookeeper_config config("10.11.21.185:21810", 3000, "/test");
+  zkclient *tmp = zkclient::open(&config);
+  ASSERT_TRUE(tmp != NULL);
+  ASSERT_TRUE(tmp->is_avaiable());
+  
+  config.set_host("no-exist:2181");
+  tmp = zkclient::open(&config);
+  ASSERT_TRUE(tmp == NULL);
+  
+  config.set_host("10.11.21.185:21810");
+  config.set_root("");
+  tmp = zkclient::open(&config);
+  ASSERT_TRUE(tmp != NULL);
+  ASSERT_TRUE(tmp->is_avaiable());
 }
